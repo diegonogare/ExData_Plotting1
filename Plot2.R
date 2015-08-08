418ab1c976a4a401081554e30fe9b4975d07fcc9
Diego Nogare <nogare@outlook.com>
2015-08-08 01:42:25 -0300

    Exploratory Data Analysis

---
+++ Plot2.R
@@ -0,0 +1,27 @@
+#set Working Directory to C:\TEMP
+setwd("C:\\Temp")
+
+#Download and unzip the dataset
+url <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <- ".\\data.zip"
+
+download.file(url, destfile, mode = "wb") 
+unzip("data.zip")
+
+#Read the Dataset and create a Datafram
+ds <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+
+#Convert column to datatype "date" and filter to specific date
+ds$Date <- as.Date(ds$Date, format="%d/%m/%Y")
+
+dates <- subset(ds, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+dates$DateTime <- paste(dates$Date, dates$Time)
+dates$DateTime <- strptime(dates$DateTime, format="%Y-%m-%d %H:%M:%S")
+
+#Plot the chart to PNG file
+png("plot2.png", width=480, height=480)
+
+plot(dates$DateTime, dates$Global_active_power, type="l", ylab="Global Activity Power (Killowatts)", xlab="")
+
+dev.off()
---
+++ Plot3.R
@@ -0,0 +1,30 @@
+#set Working Directory to C:\TEMP
+setwd("C:\\Temp")
+
+#Download and unzip the dataset
+url <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <- ".\\data.zip"
+
+download.file(url, destfile, mode = "wb") 
+unzip("data.zip")
+
+#Read the Dataset and create a Datafram
+ds <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+
+#Convert column to datatype "date" and filter to specific date
+ds$Date <- as.Date(ds$Date, format="%d/%m/%Y")
+
+dates <- subset(ds, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+dates$DateTime <- paste(dates$Date, dates$Time)
+dates$DateTime <- strptime(dates$DateTime, format="%Y-%m-%d %H:%M:%S")
+
+#Plot the chart to PNG file
+png("plot3.png", width=480, height=480)
+
+plot(dates$DateTime, dates$Sub_metering_1, type="l", ylab="Energy sub Metering", xlab="")
+lines(dates$DateTime, dates$Sub_metering_2, type="l", col="red")
+lines(dates$DateTime, dates$Sub_metering_3, type="l", col="blue")
+legend("topright", lwd=1, col=c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.off()
